1870d73e1402483f9c1e8f7496811c59fd942330
Christoph Lürig <christoph.luerig@gmail.com>
2025-02-26 20:27:36 +0100

    first rough video implementation

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,88 +1,119 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 
-namespace VideoGeoTagger
+namespace VideoGeoTagger;
+
+/// <summary>
+///     Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+
+    private readonly VideoAdministrator m_videoAdmin;
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+        m_videoAdmin = new VideoAdministrator(VideoImage, VideoSlider);
+    }
+
+
+    /// <summary>
+    ///     Gets called, when the zoom in button has been pressed
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void OnZoomIn(object sender, RoutedEventArgs e)
+    {
+    }
+
+
+    /// <summary>
+    ///     Gets called when the zoom out button has been pressed.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void OnZoomOut(object sender, RoutedEventArgs e)
+    {
+    }
+
+
+    /// <summary>
+    ///     Gets called when the mouse has been oved up un the gpx window.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void OnGpxMouseDown(object sender, MouseButtonEventArgs e)
+    {
+    }
+
+    /// <summary>
+    ///     Gets called when the mouse is released over the GPX window.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void OnGpxMouseUp(object sender, MouseButtonEventArgs e)
+    {
+    }
+
+    /// <summary>
+    ///     Gets called when the mouse is moved over the gpx window.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void OnGpxMouseMove(object sender, MouseEventArgs e)
+    {
+    }
+
+
+    /// <summary>
+    ///     Gets called when we press the load movie button.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void OnLoadVideo(object sender, RoutedEventArgs e)
+    {
+        var dialog = new OpenFileDialog
         {
-            InitializeComponent();
+            DefaultExt = ".mp4", // Default file extension
+            Filter = "Movie (.mp4)|*.mp4" // Filter files by extension
+        };
+
+        // Show open file dialog box
+        bool? result = dialog.ShowDialog();
+
+        // Process open file dialog box results
+        if (result == true)
+        {
+            m_videoAdmin.LoadVideo(dialog.FileName);
+      
+            // TODO: Here we set the movie information.
         }
+    }
 
-
-        /// <summary>
-        /// Gets called, when the zoom in button has been pressed 
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void OnZoomIn(object sender, RoutedEventArgs e)
+    /// <summary>
+    ///     Gets called when we load the gpx information.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void OnLoadGpx(object sender, RoutedEventArgs e)
+    {
+        var dialog = new OpenFileDialog
         {
+            DefaultExt = ".gpx", // Default file extension
+            Filter = "GPS Data (.gpx)|*.gpx" // Filter files by extension
+        };
 
-        }
+        // Show open file dialog box
+        bool? result = dialog.ShowDialog();
 
-
-        /// <summary>
-        /// Gets called when the zoom out button has been pressed.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void OnZoomOut(object sender, RoutedEventArgs e)
+        // Process open file dialog box results
+        if (result == true)
         {
+            // Open document
+            string filename = dialog.FileName;
 
-        }
-
-
-        /// <summary>
-        /// Gets called when the mouse has been oved up un the gpx window.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void OnGpxMouseDown(object sender, MouseButtonEventArgs e)
-        {
-
-        }
-
-        /// <summary>
-        /// Gets called when the mouse is released over the GPX window.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void OnGpxMouseUp(object sender, MouseButtonEventArgs e)
-        {
-
-        }
-
-        /// <summary>
-        /// Gets called when the mouse is moved over the gpx window.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void OnGpxMouseMove(object sender, MouseEventArgs e)
-        {
-
-        }
-
-
-        /// <summary>
-        /// Gets called, when the video slider has changed, is range from 0..1
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void OnVideoSliderChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-
+            // TODO: Here we set the movie information.
         }
     }
 }
---
+++ VideoAdministrator.cs
@@ -0,0 +1,111 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace VideoGeoTagger;
+
+public class VideoAdministrator
+{
+    private MediaClock? m_mediaClock;
+    private bool m_mediaActive;
+    private TimeSpan m_videoLength;
+    private readonly Canvas m_displayCanvas;
+    private readonly Slider m_slider;
+    private readonly Timer m_resumeTimer;
+
+
+    /// <summary>
+    /// Asks for the current video position.
+    /// </summary>
+    public TimeSpan? VideoPosition => m_mediaClock?.CurrentTime;
+
+
+    /// <summary>
+    /// Generates the video administrator from the slider and the display canvas.
+    /// </summary>
+    /// <param name="displayCanvas">The canvas where we shoe the video in.</param>
+    /// <param name="usedSlider">The slider we use.</param>
+    public VideoAdministrator(Canvas displayCanvas, Slider usedSlider)
+    {
+        m_displayCanvas = displayCanvas;
+        m_slider = usedSlider;
+        m_slider.PreviewMouseUp += SliderValueChanged;
+        m_resumeTimer = new Timer(PauseVideo);
+    }
+
+    /// <summary>
+    /// Gets invoked after some time to pause the video again.
+    /// </summary>
+    /// <param name="state"></param>
+    private void PauseVideo(object? state)
+    {
+        m_mediaClock?.Controller?.Pause();
+    }
+
+    /// <summary>
+    /// Gets called on mouse release of the slider.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void SliderValueChanged(object sender, MouseButtonEventArgs e)
+    {
+        if (m_mediaActive)
+            ProcessSliderValue();
+    }
+
+
+    /// <summary>
+    /// Called externally to load a video file.
+    /// </summary>
+    /// <param name="fileName">The name of the video file we want to load.</param>
+    public void LoadVideo(string fileName)
+    {
+        m_mediaActive = false;
+        MediaTimeline mediaTimeLine = new MediaTimeline(new Uri(fileName));
+        m_mediaClock = mediaTimeLine.CreateClock();
+        MediaPlayer mediaPlayer = new MediaPlayer
+        {
+            Clock = m_mediaClock
+        };
+        VideoDrawing drawing = new VideoDrawing
+        {
+            Player = mediaPlayer,
+            Rect = new Rect(0, 0, 1, 1)
+        };
+        m_displayCanvas.Background = new DrawingBrush(drawing);
+        m_slider.Value = 0.0f;
+        mediaPlayer.MediaOpened += MediaOpened;
+    }
+
+
+
+    /// <summary>
+    /// Callback gets invoked as soon as the video is opened.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void MediaOpened(object? sender, EventArgs e)
+    {
+        m_mediaActive = true;
+        if (m_mediaClock != null)
+            m_videoLength = m_mediaClock.NaturalDuration.TimeSpan;
+        ProcessSliderValue();
+    }
+
+    /// <summary>
+    /// We process the slider value and advance the video to the current state.
+    /// </summary>
+    private void ProcessSliderValue()
+    {
+        if (m_mediaClock == null)
+            return;
+
+        if (m_mediaClock.IsPaused)
+            m_mediaClock.Controller?.Resume();
+        TimeSpan target = m_videoLength * ( m_slider.Value / 100.0f);
+        m_mediaClock.Controller?.Seek(target, TimeSeekOrigin.BeginTime);
+        m_resumeTimer.Change(300, int.MaxValue);
+    }
+}
