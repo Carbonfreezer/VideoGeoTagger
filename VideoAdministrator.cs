8c3ea9f84c9f2b76a4e8faa58d4fa3d4d50b1097
Christoph Lürig <christoph.luerig@gmail.com>
2025-02-27 10:47:51 +0100

    hopefully final fix for video player

--- VideoAdministrator.cs
+++ VideoAdministrator.cs
@@ -1,23 +1,34 @@
-﻿using System.Diagnostics;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
 
 namespace VideoGeoTagger;
 
 public class VideoAdministrator
 {
-    private MediaPlayer? m_mediaPlayer;
-    private bool m_mediaActive;
-    private TimeSpan m_videoLength;
     private readonly Canvas m_displayCanvas;
     private readonly Slider m_slider;
-   
+    private bool m_mediaActive;
+    private MediaPlayer? m_mediaPlayer;
+    private TimeSpan m_videoLength;
+
 
     /// <summary>
-    /// Asks for the current video position or sets it.
+    ///     Generates the video administrator from the slider and the display canvas.
+    /// </summary>
+    /// <param name="displayCanvas">The canvas where we shoe the video in.</param>
+    /// <param name="usedSlider">The slider we use.</param>
+    public VideoAdministrator(Canvas displayCanvas, Slider usedSlider)
+    {
+        m_displayCanvas = displayCanvas;
+        m_slider = usedSlider;
+        m_slider.PreviewMouseUp += SliderValueChanged;
+    }
+
+
+    /// <summary>
+    ///     Asks for the current video position or sets it.
     /// </summary>
     public TimeSpan VideoPosition
     {
@@ -33,24 +44,11 @@ public class VideoAdministrator
             m_slider.Value = relativeValue * 100.0f;
             ProcessSliderValue();
         }
-    }    
- 
-
-    /// <summary>
-    /// Generates the video administrator from the slider and the display canvas.
-    /// </summary>
-    /// <param name="displayCanvas">The canvas where we shoe the video in.</param>
-    /// <param name="usedSlider">The slider we use.</param>
-    public VideoAdministrator(Canvas displayCanvas, Slider usedSlider)
-    {
-        m_displayCanvas = displayCanvas;
-        m_slider = usedSlider;
-        m_slider.PreviewMouseUp += SliderValueChanged;
     }
 
 
     /// <summary>
-    /// Gets called on mouse release of the slider.
+    ///     Gets called on mouse release of the slider.
     /// </summary>
     /// <param name="sender"></param>
     /// <param name="e"></param>
@@ -62,7 +60,7 @@ public class VideoAdministrator
 
 
     /// <summary>
-    /// Called externally to load a video file.
+    ///     Called externally to load a video file.
     /// </summary>
     /// <param name="fileName">The name of the video file we want to load.</param>
     public void LoadVideo(string fileName)
@@ -78,17 +76,11 @@ public class VideoAdministrator
         m_displayCanvas.Background = new DrawingBrush(drawing);
         m_slider.Value = 0.0f;
         m_mediaPlayer.MediaOpened += MediaOpened;
-        m_mediaPlayer.BufferingEnded += Test;
-    }
-
-    private void Test(object? sender, EventArgs e)
-    {
-        Trace.WriteLine(e);
     }
 
 
     /// <summary>
-    /// Callback gets invoked as soon as the video is opened.
+    ///     Callback gets invoked as soon as the video is opened.
     /// </summary>
     /// <param name="sender"></param>
     /// <param name="e"></param>
@@ -97,7 +89,7 @@ public class VideoAdministrator
         m_mediaActive = true;
         if (m_mediaPlayer == null)
             return;
-        
+
         m_videoLength = m_mediaPlayer.NaturalDuration.TimeSpan;
         m_mediaPlayer.SpeedRatio = 0.0000000001;
         m_mediaPlayer.Play();
@@ -105,17 +97,22 @@ public class VideoAdministrator
     }
 
     /// <summary>
-    /// We process the slider value and advance the video to the current state.
+    ///     We process the slider value and advance the video to the current state.
     /// </summary>
     private void ProcessSliderValue()
     {
         if (m_mediaPlayer == null)
             return;
-       
-        TimeSpan target = m_videoLength * ( m_slider.Value / 100.0f);
+
+        TimeSpan target = m_videoLength * (m_slider.Value / 100.0f);
         m_mediaPlayer.Position = target;
         m_mediaPlayer.Play();
-        Thread.Sleep(300);
+
+        while (m_mediaPlayer.Position == target)
+            Thread.Sleep(20);
+
         m_mediaPlayer.Pause();
+
+        m_slider.Value = 100.0f * (m_mediaPlayer.Position / m_videoLength);
     }
 }
